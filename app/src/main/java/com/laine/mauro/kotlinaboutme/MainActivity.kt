be20706118780138ff64bb6db60d959cd7f21237
Mauricio Laine <laine.mau@gmail.com>
2019-02-17 16:44:52 -0800

    added an editText and one button to show the nickname

--- app/src/main/java/com/laine/mauro/kotlinaboutme/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/kotlinaboutme/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.laine.mauro.kotlinaboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    fun addNickname(view: View) {
+        val nicknameEdit = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameText = findViewById<TextView>(R.id.nickname_text)
+        view.visibility = View.GONE
+        nicknameEdit.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+        nicknameText.text = nicknameEdit.text
+
+        //hide keyword
+        val imn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imn.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
